2e8c5fa4e3de4b5756da2e738b83fae0d5163194
Kenneth Amiel Santos <dg.kennethsantos@gmail.com>
2021-02-17 19:04:11 +0800

    Fixed Rotation

--- Assets/Scripts/HanoiRotation.cs
+++ Assets/Scripts/HanoiRotation.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 /// <summary>
 /// Component that rotates the object by _rotationOffset degress controlled by _input
@@ -11,17 +12,51 @@ public class HanoiRotation : MonoBehaviour
     [Tooltip("Rotation value to increment whenever you rotate this object")]
     [SerializeField, Range(0, 360)] private float _rotationOffset;
 
+    // How fast the object will rotate
+    [SerializeField, Range(0, 10)] private float _rotationSpeed;
+
+    // Checker to see if the object is currently rotating
+    private bool _isCurrentlyRotating = false;
+
     private void Update()
     {
-        if(_input.MoveRightPressed())
+        if(_input.MoveRightPressed() && !_isCurrentlyRotating)
         {
-            // Rotate the object by the Y-axis
-            transform.eulerAngles += transform.up * _rotationOffset;
+            Quaternion currentRotation = transform.rotation;
+            Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles - (transform.up * _rotationOffset));
+
+            _isCurrentlyRotating = true;
+            StartCoroutine(InterpolateRotation(currentRotation, targetRotation));
         }
-        else if (_input.MoveLeftPressed())
+        else if (_input.MoveLeftPressed() && !_isCurrentlyRotating)
         {
-            // Rotate the object by the Y-axis
-            transform.eulerAngles -= transform.up * _rotationOffset;
+            Quaternion currentRotation = transform.rotation;
+            Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles + (transform.up * _rotationOffset));
+
+            _isCurrentlyRotating = true;
+            StartCoroutine(InterpolateRotation(currentRotation, targetRotation));
         }
+    }
+
+    /// <summary>
+    /// Interpolates between p_startRotation and p_endRotation
+    /// </summary>
+    /// <param name="p_startRotation">Starting rotation of the object</param>
+    /// <param name="p_endRotation">Target rotation to interpolate to</param>
+    private IEnumerator InterpolateRotation(Quaternion p_startRotation, Quaternion p_endRotation)
+    {
+        // Reset the timer based on the rotation speed
+        float lerpTimer = 1f / _rotationSpeed;
+
+        // Interpolate between the start and end rotation based on the interpolation timer and rotation speed
+        while (lerpTimer > 0.0f)
+        {
+            lerpTimer -= Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(p_startRotation, p_endRotation, lerpTimer * _rotationSpeed);
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        _isCurrentlyRotating = false;
     }
 }
